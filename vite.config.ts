674d0e0a682bac752497bb179d7fae0f4dc27699
Ognjen1994 <ognjen.risti@gmail.com>
2023-07-02 23:34:09 +0200

    add config for assets in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: './',
   plugins: [legacy(), react()],
+  build: {
+    assetsDir: './assets',
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
